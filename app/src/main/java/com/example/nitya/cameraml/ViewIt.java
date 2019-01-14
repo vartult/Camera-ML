b21caa236caba337b0dedcfde414d7296f50993b
vartult <vartult@rediffmail.com>
2019-01-15 02:20:54 +0530

    custom display

--- app/src/main/java/com/example/nitya/cameraml/ViewIt.java
+++ app/src/main/java/com/example/nitya/cameraml/ViewIt.java
@@ -3,15 +3,20 @@ package com.example.nitya.cameraml;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ViewIt extends AppCompatActivity {
 
     ListView listView;
-
+    ArrayList<String> finalWord=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +25,39 @@ public class ViewIt extends AppCompatActivity {
         listView=findViewById(R.id.list);
 
         Intent intent=getIntent();
-        ArrayList<String> word=new ArrayList<>();
-        word=intent.getStringArrayListExtra("words");
 
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,word);
-        listView.setAdapter(arrayAdapter);
-
+        finalWord=intent.getStringArrayListExtra("words");
+        Log.i("msg", finalWord.get(0));
+        contentview content=new contentview();
+        listView.setAdapter(content);
     }
+
+    class contentview extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return finalWord.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemview=getLayoutInflater().inflate(R.layout.contentview,
+                    parent,
+                    false);
+            TextView contentview=itemview.findViewById(R.id.content);
+            contentview.setText(getItem(position));
+            return  itemview;
+        }
+    }
+
 }
